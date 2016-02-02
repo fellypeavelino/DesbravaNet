4b51e16045ff539208ff538087d21c4114c033f5
Rafael Cardoso Tavares <rafael.tavares@anpix.com.br>
2016-02-02 08:55:19 -0200

    DB Backup
    DB Backup

---
+++ sql.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 02-Fev-2016 às 11:51
+-- Versão do servidor: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `code`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users_groups`
+--
+
+CREATE TABLE IF NOT EXISTS `users_groups` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Extraindo dados da tabela `users_groups`
+--
+
+INSERT INTO `users_groups` (`id`, `name`) VALUES
+(2, 'Administrador'),
+(1, 'Criador'),
+(3, 'Desbravador'),
+(4, 'Diretor'),
+(5, 'Distrital'),
+(6, 'Regional');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user` varchar(50) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `registered` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `user` (`user`),
+  KEY `group_id` (`group_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `user`, `name`, `group_id`, `pass`, `email`, `registered`) VALUES
+(1, 'anpix', 'Rafael Tavares', 1, '8f5a236b92d2236ceb61d770054cc03f', 'rafael.tavares@anpix.com.br', '2016-01-30 15:31:17'),
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
