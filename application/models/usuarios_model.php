1f1e3137c44e69a28a65b24d73a3fa4d9e09e376
Rafael Cardoso Tavares <rafael.tavares@anpix.com.br>
2016-02-02 12:42:15 -0200

    Easy table rename

--- application/models/usuarios_model.php
+++ application/models/usuarios_model.php
@@ -1,19 +1,20 @@
 <?php
 
 Class Usuarios_model extends CI_Model {
+	private $table = "usuarios";
 
 	public function salvar($usuario){
-		$this->db->insert("usuarios",$usuario);
+		$this->db->insert($table,$usuario);
 	}
 
 
 	public function searchByUser($user){
 		$this->db->where("user",$user);
-		return $this->db->get("usuarios")->row_array();	//apenas uma linha
+		return $this->db->get($table)->row_array();	//apenas uma linha
 	}
 
 	public function getAll(){
-		return $this->db->get("usuarios")->result_array();	//Todos os registros
+		return $this->db->get($table)->result_array();	//Todos os registros
 	}
 
 	//$query = "INSERT INTO tbl_chatberichten (chat_id, student_id, bericht) VALUES (?,?,?)";
